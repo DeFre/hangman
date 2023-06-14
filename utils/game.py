1defbbf17ba8412f2ddb85bbff599a621571b38d
DeFre <DeFre@users.noreply.github.com>
2023-06-14 11:02:34 +0200

    started defining attributes

---
+++ utils/game.py
@@ -0,0 +1,24 @@
+
+class hangman:
+    """
+    Class that defines everything we need for our game
+    """
+
+    def __init__(self):
+        """when the game starts, it picks a word and sets the number of lives to 5"""
+        self
+    
+    def possible_words(self):
+        """Contains the list of words the game can choose from"""
+
+    def word_to_find(self): 
+        """a list containing all the letters of the word which need to be guessed as strings"""
+
+    def lives(self):
+        """the number of lives a player still has left"""
+        self.lives = 5
+
+    def correctly_guessed_letters(self):
+        """a list of strings where each element will be a letter guessed by the user. At the start, it should be equal to: `_ _ _ _ _`, with the same number of `_` as the length of the word to find. """
+
+
